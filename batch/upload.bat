99bc3c0be8f4c59ba153484dac5ff12de852460a
Thomas Breidenbach <breidenbach.thomas@gmail.com>
2018-10-21 01:07:51 +0200

    adds automatic led server start ID: 0203491

--- batch/upload.bat
+++ batch/upload.bat
@@ -2,7 +2,7 @@ pscp.exe -pw openhabian ..\openhabkonfig\items\*.items openhabian@filderkoffer:/
 
 pscp.exe -pw openhabian ..\openhabkonfig\sitemaps\*.sitemap openhabian@filderkoffer:/etc/openhab2/sitemaps 
 
-::pscp.exe -pw openhabian c:\Users\Thomas\Documents\openhab\goethestrasse\*.things openhabian@filderkoffer:/etc/openhab2/things 
+pscp.exe -pw openhabian ..\openhabkonfig\things\*.things openhabian@filderkoffer:/etc/openhab2/things 
 
 pscp.exe -pw openhabian ..\openhabkonfig\rules\*.rules openhabian@filderkoffer:/etc/openhab2/rules
 
